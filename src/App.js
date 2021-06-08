bea659076e350f3ebafdaf975fe80303da186252
harold parada <haroldparada20@gmail.com>
2021-06-07 21:19:30 -0400

    added nav bar

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Box, Button, Collapsible, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+function App() {
+
+  const theme = {
+    global: {
+      colors: {
+             brand: '#228BE6',
+           },
+      font: {
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
+      },
+    },
+  };
+   
+  const AppBar = (props) => (
+      <Box
+        tag='header'
+        direction='row'
+        align='center'
+        justify='between'
+        background='brand'
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation='medium'
+        style={{ zIndex: '1' }}
+        {...props}
+      />
+    );
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+  <Grommet theme={theme} full>
+    <Box fill>
+      <AppBar>
+      <Heading level='3' margin='none'>My App</Heading>
+      <Button
+        icon={<Notification />}
+        onClick={() => setShowSidebar(!showSidebar)}
+      />
+      </AppBar>
+      <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+       <Box flex align='center' justify='center'>
+          app body
+       </Box>
+       <Collapsible direction="horizontal" open={showSidebar}>
+           <Box
+             flex
+             width='medium'
+             background='light-2'
+             elevation='small'
+             align='center'
+             justify='center'
+           >
+             sidebar
+           </Box>
+        </Collapsible>
+      </Box>
+      </Box>
+  </Grommet>
+  );
+}
+
+export default App;
