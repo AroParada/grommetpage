c638fd52e870e542d6b59ab046a1b17d9076dbe6
harold parada <haroldparada20@gmail.com>
2021-06-11 00:24:33 -0400

    made a card component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { Box, Button, Collapsible, Heading, Grommet, ResponsiveContext, Layer } from 'grommet';
 import { FormClose, Notification } from 'grommet-icons';
+import MyCard from './components/card'
+import Typical from 'react-typical'
 
 function App() {
 
   const theme = {
     global: {
       colors: {
-             brand: '#228BE6',
-           },
+        brand: '#00739D',
+      },
       font: {
         family: 'Roboto',
         size: '18px',
@@ -17,78 +19,9 @@ function App() {
     },
   };
    
-  const AppBar = (props) => (
-      <Box
-        tag='header'
-        direction='row'
-        align='center'
-        justify='between'
-        background='brand'
-        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-        elevation='medium'
-        style={{ zIndex: '1' }}
-        {...props}
-      />
-    );
-
-  const [showSidebar, setShowSidebar] = useState(false);
   return (
   <Grommet theme={theme} full>
-   <ResponsiveContext.Consumer>
-   {size => (
-    <Box fill>
-      <AppBar>
-      <Heading level='3' margin='none'>My App</Heading>
-      <Button
-        icon={<Notification />}
-        onClick={() => setShowSidebar(!showSidebar)}
-      />
-      </AppBar>
-      <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-       <Box flex align='center' justify='center'>
-          app body
-       </Box>
-       {(!showSidebar || size !== 'small') ? (
-        <Collapsible direction="horizontal" open={showSidebar}>
-           <Box
-             flex
-             width='medium'
-             background='light-2'
-             elevation='small'
-             align='center'
-             justify='center'
-           >
-             sidebar
-           </Box>
-        </Collapsible>
-        ): (
-        <Layer>
-        <Box
-          background='light-2'
-          tag='header'
-          justify='end'
-          align='center'
-          direction='row'
-         >
-          <Button
-          icon={<FormClose />}
-          onClick={() => setShowSidebar(false)}
-         />
-         </Box>
-          <Box
-            fill
-            background='light-2'
-            align='center'
-            justify='center'
-            >
-            sidebar
-          </Box>
-        </Layer>
-        )}
-      </Box>
-      </Box>
-      )}
-      </ResponsiveContext.Consumer>
+  <MyCard/>
   </Grommet>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Avatar, Button, Card, CardHeader, CardBody, CardFooter } from 'grommet';
+import { UserFemale, Favorite, ShareOption } from 'grommet-icons'
+
+export default () => (
+<Card  height="medium" width="medium" background="light-1">
+      <CardHeader pad="medium">Header</CardHeader>
+      <CardBody 
+        pad="medium">Body
+        <Box justify="center" direction="row" gap="small">
+        <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />    
+        </Box>
+      </CardBody>
+      <CardFooter pad={{horizontal: "small"}} background="light-2">   
+      <Button
+      icon={<Favorite color="red" />}
+      hoverIndicator
+    />
+    <Button icon={<ShareOption color="plain" />} hoverIndicator />
+    </CardFooter>
+</Card>
+)
