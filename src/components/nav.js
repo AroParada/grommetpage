cb8fc472a84bbc5a6d99d529dea3c7f9697fea32
harold parada <haroldparada20@gmail.com>
2021-06-29 02:03:13 -0400

    made nav resonsive

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { Nav, Anchor, Text } from 'grommet';
-import { Mail, Github, Linkedin, DocumentPdf, } from 'grommet-icons'
+import { Nav, Anchor, Text, ResponsiveContext, Header, Box, Menu } from 'grommet';
+import { Home, Mail, Github, Linkedin, DocumentPdf, } from 'grommet-icons'
 
-function Mynav() {
+const Mynav = () => {
 
-  const theme = {
-    global: {
-      colors: {
-        brand: '#00739D',
-      },
-      font: {
-        family: 'Roboto',
-        size: '18px',
-        height: '20px',
-      },
-    },
-  };
+  // const theme = {
+  //   global: {
+  //     colors: {
+  //       brand: '#00739D',
+  //     },
+  //     font: {
+  //       family: 'Roboto',
+  //       size: '18px',
+  //       height: '20px',
+  //     },
+  //   },
+  // };
 
   return (
-    <Nav direction="row" background="brand" pad="medium">
-    <Text color="white" alignSelf="center" size="xlarge" >
-          Aro Parada
-    </Text>
+<Header background="brand" pad="medium">
+      <Box direction="row" align="center" gap="small">
+        <Text color="white" alignSelf="center" size="xlarge" >
+           Aro Parada
+        </Text>
+      </Box>
+      <ResponsiveContext.Consumer>
+        {responsive =>
+          responsive === 'small' ? (
+            <Menu
+              label=""
+              items={[
+                { icon: <Home />, onClick: () => {} },
+                { icon: <Linkedin />, onClick: () => {} },
+                { icon: <DocumentPdf />, onClick: () => {} },
+                { icon: <Mail />, onClick: () => {} },
+                { label: 'Portfolio', onClick: () => {} },
+              ]}
+            />
+          ) : (
+  <Nav direction="row" background="brand" pad="medium">
     <Anchor icon={<Github />} hoverIndicator />
     <Anchor icon={<Linkedin />} hoverIndicator />
     <Anchor icon={<DocumentPdf />} hoverIndicator />
-    <Anchor icon={<Mail />} hoverIndicator />
-    
-</Nav>
+    <Anchor icon={<Mail />} hoverIndicator />  
+  </Nav>
+      )
+    }
+  </ResponsiveContext.Consumer>
+</Header>
   )
 }
 export default Mynav;
