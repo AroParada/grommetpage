90fbc4bd91ab6af5fe634744fe17486543617b41
harold parada <haroldparada20@gmail.com>
2021-07-06 01:58:11 -0400

    added more react typical to intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Paragraph, grommet, Box, Button, Grommet } from 'grommet';
+import Typical from 'react-typical'
+import { Pause } from 'grommet-icons';
 
 
 const Intro = () => {
@@ -7,6 +9,23 @@ const Intro = () => {
     return (
     <Grommet theme={grommet}>
         <Box align="center" pad="">
+        <Typical
+              text-align="center"
+              align="center"
+              steps={[
+                "",
+                1200 ,
+                "Web Developer💻",
+                2000 ,
+                "musician 🎸",
+                1000 ,
+                "history nerd 📚",
+                1000,
+                "food lover 🍔", 
+                ]}
+              loop={Infinity}
+              wrapper="h1"
+            /> 
         <Paragraph margin="none">
            Lorem ipsum dolor sit amet,
            consectetur adipiscing elit,
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {grommet, Grommet} from 'grommet';
-import {Js, Reactjs, Node, Npm, Html5} from 'grommet-icons'
+import {Js, Reactjs, Node, Npm, Html5, Css3, Wordpress, Gremlin, Heroku} from 'grommet-icons'
 
 const Skills = () => {
 
     return(
     <Grommet background="clear" theme={grommet}>
+        <Html5 color='plain' size='large'/>
+        <Css3 color='#0daeff' size='large'/>
         <Js color='plain' size='large' />
         <Reactjs color='plain' size='large'/>
         <Node color='plain' size='large'/>
         <Npm color='plain' size='large'/>
-        <Html5 color='plain' size='large'/>
-
+        <Wordpress color='plain' size='large'/>
+        <Gremlin color='purple' size='large'/>
+        <Heroku color='plain' size='large'/>
     </Grommet>
 
     )
