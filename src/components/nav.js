1dd35577eec6bab81f171cd45626970b34b1e9a4
Aro Parada <haroldparada20@gmail.com>
2021-11-09 02:38:28 -0500

    removed home button

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ const Mynav = () => {
   return (
 <Header background="brand" pad="x-small">
       <Box direction="row" align="center" gap="small">
-        <Anchor href="/grommetpage/#/home" color="white" alignSelf="center" size="xlarge" label="A" margin="medium" weight="" >  
+        <Anchor color="white" alignSelf="center" size="xlarge" label="A" margin="medium" weight="" >  
         </Anchor>
       </Box>
       <ResponsiveContext.Consumer>
@@ -20,7 +20,7 @@ const Mynav = () => {
               justifyContent="center"
               items=
               {[
-                { icon: <Anchor href="/grommetpage/#/home" color="white" icon={<Home />} />, onClick: () => {} },
+                // { icon: <Anchor href="/grommetpage/#/home" color="white" icon={<Home />} />, onClick: () => {} },
                 { icon: <Anchor href="https://github.com/AroParada" target="_blank" rel="noopener" color="white" icon={<Github />} />,  onClick: () => {} },
                 { icon: <Anchor href="https://docs.google.com/document/d/19dAEEabihDZaa6nlFYliqCUL_HK2jThcuAPnxInU194/edit?usp=sharing" target="_blank" rel="noopener" color="white" icon={<Document />} />, onClick: () => {} },
                 { icon: <Anchor href="mailto:haroldparada20@gmail.com" color="white" icon={<Mail />} />, onClick: () => {} },
@@ -28,7 +28,7 @@ const Mynav = () => {
             />
           ) : (
   <Nav direction="row" background="brand" pad="medium">
-    <Anchor href="/grommetpage/#/home" color="white" icon={<Home />} hoverIndicator /> 
+    {/* <Anchor href="/grommetpage/#/home" color="white" icon={<Home />} hoverIndicator />  */}
     <Anchor href="https://github.com/AroParada" target="_blank" rel="noopener" color="white" icon={<Github />} hoverIndicator />
     <Anchor href="https://www.linkedin.com/in/aroparada/" target="_blank" rel="noopener" color="white" icon={<Linkedin />} hoverIndicator />
     <Anchor href="mailto:haroldparada20@gmail.com" color="white" icon={<Mail />} hoverIndicator />  
